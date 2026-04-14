dde26a2bc547355b7c463f0007faef03d3701f05
MohamedAshrafzZ <mashrafzz957@gmail.com>
2026-04-14 16:54:42 +0200

    Question 1

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿
 using G_NET_12_LINQ02.DataSource;
+using G_NET_12_LINQ02.Models;
 
 
 
@@ -9,7 +10,18 @@ namespace G_NET_12_LINQ02
     {
         static void Main(string[] args)
         {
-            
+            #region Question 1 
+            //1.Get top 3 most expensive products
+
+            //Answer:
+            var result = Source.ProductList.OrderByDescending(p => p.UnitPrice).Take(3).Select(p => p.ProductName );
+
+
+            #endregion
+            foreach (var item in result )
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
