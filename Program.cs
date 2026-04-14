d81f17d8ba119a3c3e71e968d9a4c4541300d345
MohamedAshrafzZ <mashrafzz957@gmail.com>
2026-04-14 16:51:00 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿
+using G_NET_12_LINQ02.DataSource;
+
+
+
+namespace G_NET_12_LINQ02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
